29b7676cd4f7a5f3ca73f1bf11e140e12a4ed296
Kubanych <talipovkuba555@gmail.com>
2022-05-28 23:46:47 +0600

    django project

---
+++ generator/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+def home(request):
+    return render(request, 'generator/home.html')
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%&'))
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+
+
+    length = int(request.GET.get('length'))
+
+    thepassword = ''
+
+    for i in range(length):
+        thepassword += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password':thepassword})
